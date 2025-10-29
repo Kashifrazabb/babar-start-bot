4d34039dde14adfcadf52cf671f3e807b2faa7f6
kashifrazabb <kashifrazabb@gmail.com>
2025-10-29 17:31:05 +0500

    test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import TelegramBot from 'node-telegram-bot-api';
 
-const token = process.env.TOKEN;
+const token = "8339068809:AAHR-oZTuvnqwym9-5U01uM3GAQTZZp0BcY";
 
 const bot = new TelegramBot(token, { polling: true });
 
